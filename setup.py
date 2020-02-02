763db7a48fbc8215c425cc7cb07c114290ca6d59
aaron-burgess <56049510+aaron-burgess@users.noreply.github.com>
2020-02-02 15:50:21 -0500

    Updated to represent breaking release with 1.0.x version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rabbit-clients',
-    version='1.0.0',
+    version='2.0.0',
     packages=['tests', 'rabbit_clients', 'rabbit_clients.clients'],
     url='https://github.com/awburgess/rabbit-clients',
     license='MIT License',
