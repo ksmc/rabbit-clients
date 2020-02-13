08a99d79ce11e713116258acaa837a50cdb8fcfd
aaron-burgess <56049510+aaron-burgess@users.noreply.github.com>
2020-02-13 10:48:13 -0500

    Updated version for PyPI
    Do this so that we represent a break in backwards compatibility if the user was setting env vars

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rabbit-clients',
-    version='2.2.1',
+    version='3.0.0',
     packages=['tests', 'rabbit_clients', 'rabbit_clients.clients'],
     url='https://github.com/awburgess/rabbit-clients',
     license='MIT License',
